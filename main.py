Fixed memory overflow and added variable filter size

main.py
@@ -1,6 +1,5 @@
 import argparse
 import math
-import os
 from datetime import datetime
 from os import listdir
 from os.path import isfile, join
@@ -87,7 +86,8 @@ def conv_pool_layer_with_bias(input, shape, name=None, is_training=True):
         biases = bias_variable([shape[3]], name=name + "_biases")
         bias = tf.nn.bias_add(c, biases)
         with tf.device('/cpu:0'):
-            conv = tf.nn.relu(tf.contrib.layers.batch_norm(bias, is_training=is_training, center=False, scope=name + "_bn"))
+            conv = tf.nn.relu(
+                tf.contrib.layers.batch_norm(bias, is_training=is_training, center=False, scope=name + "_bn"))
     return max_pool_2x2_argmax(conv, name=name + "_pool")
 
 
@@ -113,6 +113,7 @@ def msra_initializer(kl, dl):
 def loss(logits, labels):
     with tf.name_scope('loss'):
         tf.cast(labels, tf.int32)
+        # reshape logits to 1-D array of one hot vectors
         logits = tf.reshape(logits, (-1, NUM_CLASSES))
         epsilon = tf.constant(value=1e-10, name="epsilon")
         logits = tf.add(logits, epsilon)
@@ -120,8 +121,11 @@ def loss(logits, labels):
         # one hot label
         label_flat = tf.reshape(labels, (-1, 1))
         with tf.device("/cpu:0"):
-            labels = tf.reshape(tf.one_hot(label_flat, depth=32), (-1, 32))
+            # reshape labels to the same as logits
+            labels = tf.reshape(tf.one_hot(label_flat, depth=NUM_CLASSES), (-1, NUM_CLASSES))
+        # apply softmax on logits to extract label possibilities
         softmax = tf.nn.softmax(logits)
+        # compute the cross entropy of logits vs labels
         cross_entropy = -tf.reduce_sum(labels * tf.log(softmax + epsilon), reduction_indices=[1])
         cross_entropy_mean = tf.reduce_mean(cross_entropy, name="cross_entropy")
     return cross_entropy_mean
@@ -140,12 +144,16 @@ def generate_batch(image, label, min_ex, batch_size, shuffle):
 
 
 def reader(queue):
+    # file names
     imageName = queue[0]
     labelName = queue[1]
+    # read bytes into tensor
     imageVal = tf.read_file(imageName)
     labelVal = tf.read_file(labelName)
+    # convert from png format
     imageB = tf.image.decode_png(imageVal)
     labelB = tf.image.decode_png(labelVal)
+    # reshape to image/label shape
     image = tf.reshape(imageB, (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH))
     label = tf.reshape(labelB, (IMAGE_HEIGHT, IMAGE_WIDTH, 1))
     return image, label
@@ -162,99 +170,151 @@ def CamVid(images, labels, batch_size):
     return generate_batch(reshaped, label, min_ex, batch_size, shuffle=True)
 
 
-def main(imageN, labelN, args):
-    global_step = tf.Variable(0, trainable=False)
-    imgs, labels = CamVid(imageN, labelN, BATCH_SIZE)
-    with tf.device('/gpu:0'):
-        x = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT, IMAGE_WIDTH, 3], name="x")
-        y_ = tf.placeholder(tf.uint8, shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, 1], name="y_")
-        normal = tf.nn.lrn(x, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75, name="normalize")
-        pool1, pool1_indices = conv_pool_layer_with_bias(normal, [7, 7, IMAGE_DEPTH, 64], name="pool1", is_training=args.train)
-        pool2, pool2_indices = conv_pool_layer_with_bias(pool1, [7, 7, 64, 64], name="pool2", is_training=args.train)
-        pool3, pool3_indices = conv_pool_layer_with_bias(pool2, [7, 7, 64, 64], name="pool3", is_training=args.train)
-        pool4, pool4_indices = conv_pool_layer_with_bias(pool3, [7, 7, 64, 64], name="pool4", is_training=args.train)
+def load_names(train=True):
+    if train:
+        train_dir = "CamVid/train/"
+        annot = "CamVid/trainannot/"
+        images = [train_dir + f for f in listdir(train_dir) if isfile(join(train_dir, f))]
+        annotated = [annot + f for f in listdir(annot) if isfile(join(annot, f))]
+    else:
+        test_dir = "CamVid/test/"
+        annot = "CamVid/testannot/"
+        images = [test_dir + f for f in listdir(test_dir) if isfile(join(test_dir, f))]
+        annotated = [annot + f for f in listdir(annot) if isfile(join(annot, f))]
+    assert (images[0][:10] == annotated[0][:10])
+    return images, annotated
 
-        up4 = deconv_layer(pool4, [2, 2, 64, 64], [BATCH_SIZE, IMAGE_HEIGHT // 8, IMAGE_WIDTH // 8, 64], name="up4")
-        de4 = conv_layer_with_bias(up4, [7, 7, 64, 64], name="de4", is_training=args.train)
-        up3 = deconv_layer(de4, [2, 2, 64, 64], [BATCH_SIZE, IMAGE_HEIGHT // 4, IMAGE_WIDTH // 4, 64], name="up3")
-        de3 = conv_layer_with_bias(up3, [7, 7, 64, 64], name="de3", is_training=args.train)
-        up2 = deconv_layer(de3, [2, 2, 64, 64], [BATCH_SIZE, IMAGE_HEIGHT // 2, IMAGE_WIDTH // 2, 64], name="up2")
-        de2 = conv_layer_with_bias(up2, [7, 7, 64, 64], name="de2", is_training=args.train)
-        up1 = deconv_layer(de2, [2, 2, 64, 64], [BATCH_SIZE, IMAGE_HEIGHT, IMAGE_WIDTH, 64], name="up1")
-        de1 = conv_layer_with_bias(up1, [7, 7, 64, 64], name="de1", is_training=args.train)
-        with tf.variable_scope('conv_classifier'):
-            kernel = tf.get_variable('weights', [1, 1, 64, 32], initializer=msra_initializer(1, 64))
-            conv = tf.nn.conv2d(de1, kernel, [1, 1, 1, 1], padding='SAME', name="conv")
-            biases = bias_variable([32], name="conv_biases")
-            conv_classifier = tf.nn.bias_add(conv, biases, name="conv_classifier")
-            mean_loss = loss(conv_classifier, y_)
-        opt = tf.train.AdamOptimizer(args.lr).minimize(mean_loss, global_step=global_step)
-    # Add histograms for trainable variables.
-    for var in tf.trainable_variables():
-        tf.histogram_summary(var.op.name, var)
-    correct = tf.equal(
-        tf.reshape(tf.argmax(tf.slice(conv_classifier, [0, 0, 0, 0], [1, -1, -1, -1]), 3), [-1, 360, 480, 1]),
-        tf.cast(tf.slice(y_, [0, 0, 0, 0], [1, -1, -1, -1]), tf.int64))
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-    tf.summary.image('annotated', y_)
-    tf.summary.image('correct', tf.cast(tf.reshape(correct, [-1, 360, 480, 1]), tf.uint8) * 255)
-    tf.summary.image('input', x)
-    tf.summary.image('output',
-                     tf.cast(tf.reshape(tf.argmax(tf.slice(conv_classifier, [0, 0, 0, 0], [1, -1, -1, -1]), 3),
-                                        [-1, 360, 480, 1]),
-                             tf.uint8) * 7)
-    tf.summary.scalar('accuracy', accuracy)
-    print("Done setting up graph.")
-    tf.summary.scalar('loss', mean_loss)
-    tf.summary.histogram('biases', biases)
-    saver = tf.train.Saver(tf.global_variables())
-    merged = tf.summary.merge_all()
+
+def setup(args, filter_size=16):
+    x = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT, IMAGE_WIDTH, 3], name="x")
+    y_ = tf.placeholder(tf.uint8, shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, 1], name="y_")
+    normal = tf.nn.lrn(x, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75, name="normalize")
+    pool1, pool1_indices = conv_pool_layer_with_bias(normal, [7, 7, IMAGE_DEPTH, filter_size], name="pool1",
+                                                     is_training=args.train)
+    pool2, pool2_indices = conv_pool_layer_with_bias(pool1, [7, 7, filter_size, filter_size], name="pool2",
+                                                     is_training=args.train)
+    pool3, pool3_indices = conv_pool_layer_with_bias(pool2, [7, 7, filter_size, filter_size], name="pool3",
+                                                     is_training=args.train)
+    pool4, pool4_indices = conv_pool_layer_with_bias(pool3, [7, 7, filter_size, filter_size], name="pool4",
+                                                     is_training=args.train)
+
+    up4 = deconv_layer(pool4, [2, 2, filter_size, filter_size], [BATCH_SIZE, IMAGE_HEIGHT // 8, IMAGE_WIDTH // 8, filter_size],
+                       name="up4")
+    de4 = conv_layer_with_bias(up4, [7, 7, filter_size, filter_size], name="de4", is_training=args.train)
+    up3 = deconv_layer(de4, [2, 2, filter_size, filter_size], [BATCH_SIZE, IMAGE_HEIGHT // 4, IMAGE_WIDTH // 4, filter_size],
+                       name="up3")
+    de3 = conv_layer_with_bias(up3, [7, 7, filter_size, filter_size], name="de3", is_training=args.train)
+    up2 = deconv_layer(de3, [2, 2, filter_size, filter_size], [BATCH_SIZE, IMAGE_HEIGHT // 2, IMAGE_WIDTH // 2, filter_size],
+                       name="up2")
+    de2 = conv_layer_with_bias(up2, [7, 7, filter_size, filter_size], name="de2", is_training=args.train)
+    up1 = deconv_layer(de2, [2, 2, filter_size, filter_size], [BATCH_SIZE, IMAGE_HEIGHT, IMAGE_WIDTH, filter_size], name="up1")
+    de1 = conv_layer_with_bias(up1, [7, 7, filter_size, filter_size], name="de1", is_training=args.train)
+    with tf.variable_scope('conv_classifier'):
+        kernel = tf.get_variable('weights', [1, 1, filter_size, 32], initializer=msra_initializer(1, 64))
+        conv = tf.nn.conv2d(de1, kernel, [1, 1, 1, 1], padding='SAME', name="conv")
+        biases = bias_variable([32], name="conv_biases")
+        conv_classifier = tf.nn.bias_add(conv, biases, name="conv_classifier")
+        classifier = tf.nn.softmax(conv_classifier)
+        mean_loss = loss(classifier, y_)
+    return x, y_, classifier, mean_loss
+
+
+def test(args, global_step):
+    im, an = load_names(train=False)
+    with tf.device('/cpu:0'):
+        imgs, labels = CamVid(im, an, BATCH_SIZE)
+    x, y_, conv_classifier, mean_loss = setup(args)
+    variable_averages = tf.train.ExponentialMovingAverage(0.9999, global_step)
+    variable_averages.apply(tf.trainable_variables())
     now = datetime.now()
-    with tf.device('/gpu:0'):
-        with tf.Session() as sess:
-            train_writer = tf.summary.FileWriter('train/' + now.strftime("%Y%m%d-%H%M%S") + "/", sess.graph)
-            test_writer = tf.summary.FileWriter('test')
-            print("Step")
-            init = tf.global_variables_initializer()
-            sess.run(init)
+    LOG_DIR = 'test/' + now.strftime("%Y%m%d-%H%M%S") + "/"
+    with tf.Session() as sess:
+        with tf.device('/cpu:0'):
+            variable_averages = tf.train.ExponentialMovingAverage(
+                0.9999)
+            variables_to_restore = variable_averages.variables_to_restore()
+            saver = tf.train.Saver(variables_to_restore)
+        ckpt = tf.train.get_checkpoint_state(args.check)
+        if ckpt and ckpt.model_checkpoint_path:
+            print(ckpt.model_checkpoint_path)
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print("No checkpoint found! Please specify using the argument -ckpt")
+            return
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        for step in range(args.max):
+            imageB, labelB = sess.run([imgs, labels])
+            feed = {x: imageB, y_: labelB}
+            pred = sess.run([conv_classifier], feed_dict=feed)
 
-            if args.train:
-
-
-                coord = tf.train.Coordinator()
-                threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-                for step in range(args.max):
-                    imageB, labelB = sess.run([imgs, labels])
-                    feed = {x: imageB, y_: labelB}
-                    _, l, a, summary = sess.run([opt, mean_loss, accuracy, merged], feed_dict=feed)
-                    print(step, l, a)
-                    if step % 10 == 0:
-                        train_writer.add_summary(summary, step)
-                    if step % 500 == 0 or (step + 1) == args.max:
-                        saver.save(sess, join(os.curdir, 'model.ckpt'), global_step=global_step)
-            else:
-                ckpt = tf.train.get_checkpoint_state(os.curdir)
-                if ckpt and ckpt.model_checkpoint_path:
-                    print(ckpt.model_checkpoint_path)
-                    saver.restore(sess, ckpt.model_checkpoint_path)
-                else:
-                    print("No checkpoint found! Please specify using the argument -ckpt")
-                    return
-                coord = tf.train.Coordinator()
-                threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-                for step in range(args.max):
-                    imageB, labelB = sess.run([imgs, labels])
-                    feed = {x: imageB, y_: labelB}
-                    logits, l, a, summary = sess.run([conv_classifier, mean_loss, accuracy, merged], feed_dict=feed)
-                    print(step, l, a)
-                    if step % 10 == 0:
-                        test_writer.add_summary(summary, step)
-
-            coord.request_stop()
-            coord.join(threads)
+        coord.request_stop()
+        coord.join(threads)
 
 
-MAX_STEPS = 30000
+def train(args, global_step):
+    im, an = load_names(True)
+
+    x, y_, conv_classifier, mean_loss = setup(args)
+    opt = tf.train.AdamOptimizer(args.lr).minimize(mean_loss, global_step=global_step)
+    variable_averages = tf.train.ExponentialMovingAverage(0.9999, global_step)
+    var_op = variable_averages.apply(tf.trainable_variables())
+    with tf.control_dependencies([opt, var_op]):
+        train_op = tf.no_op(name="train")
+    with tf.device('/cpu:0'):
+        # Add histograms for trainable variables.
+        for var in tf.trainable_variables():
+            tf.summary.histogram(var.op.name, var)
+        correct = tf.equal(
+            tf.reshape(tf.argmax(tf.slice(conv_classifier, [0, 0, 0, 0], [1, -1, -1, -1]), 3), [-1, 360, 480, 1]),
+            tf.cast(tf.slice(y_, [0, 0, 0, 0], [1, -1, -1, -1]), tf.int64))
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+        tf.summary.image('annotated', y_)
+        tf.summary.image('correct', tf.cast(tf.reshape(correct, [-1, 360, 480, 1]), tf.uint8) * 255)
+        tf.summary.image('input', x)
+        tf.summary.image('output',
+                         tf.cast(tf.reshape(tf.argmax(tf.slice(conv_classifier, [0, 0, 0, 0], [1, -1, -1, -1]), 3),
+                                            [-1, 360, 480, 1]),
+                                 tf.uint8) * 7)
+        tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('loss', mean_loss)
+
+    LOG_DIR = 'train/' + datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+
+    print("Done setting up graph.")
+    with tf.device('/cpu:0'):
+        saver = tf.train.Saver(tf.global_variables())
+        # load batch
+        imgs, labels = CamVid(im, an, BATCH_SIZE)
+        merged = tf.summary.merge_all()
+    with tf.Session() as sess:
+        train_writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        for step in range(args.max):
+            imageB, labelB = sess.run([imgs, labels])
+            feed = {x: imageB, y_: labelB}
+            _, l, a, summary = sess.run([train_op, mean_loss, accuracy, merged], feed_dict=feed)
+            print(step, l, a)
+            if step % 10 == 0:
+                train_writer.add_summary(summary, step)
+            if step % 500 == 0 or (step + 1) == args.max:
+                saver.save(sess, join(LOG_DIR, 'model.ckpt'), global_step=step)
+        coord.request_stop()
+        coord.join(threads)
+
+
+def main(args):
+    global_step = tf.Variable(0, trainable=False)
+    if args.train:
+        train(args, global_step)
+    else:
+        test(args, global_step)
+
+
+MAX_STEPS = 1000
 NUM_CLASSES = 32
 LEARNING_RATE = 1e-4
 
@@ -263,16 +323,6 @@ if __name__ == "__main__":
     parser.add_argument("--train", action="store_true", help="Train a new model")
     parser.add_argument("-lr", type=float, default=LEARNING_RATE, help="Learning rate")
     parser.add_argument("-max", type=int, default=MAX_STEPS, help="Max number of steps to take")
+    parser.add_argument("-check", type=str, default=".", help="Checkpoint directory.")
     args = parser.parse_args()
-    if args.train:
-        train = "CamVid/train/"
-        annot = "CamVid/trainannot/"
-        images = [train + f for f in listdir(train) if isfile(join(train, f))]
-        annotated = [annot + f for f in listdir(annot) if isfile(join(annot, f))]
-        assert (images[0][:10] == annotated[0][:10])
-    else:
-        test = "CamVid/test/"
-        annot = "CamVid/testannot/"
-        images = [test + f for f in listdir(test) if isfile(join(test, f))]
-        annotated = [annot + f for f in listdir(annot) if isfile(join(annot, f))]
-    main(images, annotated, args)
+    main(args)